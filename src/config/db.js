5bde8cfe5f6980bed858872f3a7070d5b2ce12ef
Chiragjethwa6 <c.jethwa@somaiya.edu>
2025-03-27 01:38:14 -0400

    updated mongo connection

--- src/config/db.js
+++ src/config/db.js
@@ -2,14 +2,17 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.!MONGO_URI);
-        console.log(`MongoDB Connected`);
-    } catch (err) {
-        console.error("Database connection failed", err);
-        process.exit(1);
-    }
-}
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB Connected');
+  } catch (error) {
+    console.error('Database connection failed', error);
+    process.exit(1);
+  }
+};
 
 const disconnectDB = async () => {
     await mongoose.disconnect();
